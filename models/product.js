ad9dac84f8c67b2f2deab0f622364cf512922a83
chathura <chathuraasela11@gmail.com>
2025-02-21 16:43:12 +0530

    added functions to update and delete

--- models/product.js
+++ models/product.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 
 const productSchema = new mongoose.Schema({
+    key:{
+        type:String,
+        required:true,
+        unique:true
+    },
     name:{
         type:String,
         required:true
@@ -10,9 +15,28 @@ const productSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    category:{
+        type:String,
+        required:true,
+        default:"uncategorized"
+    },
+    dimensions:{
+        type:String,
+        required:true
+    },
     description:{
         type:String,
         required:true
+    },
+    availability:{
+        type:Boolean,
+        required:true,
+        default:true
+    },
+    image:{
+        type:[String],
+        required:true,
+        default:["https://apollobattery.com.au/wp-content/uploads/2022/08/default-product-image.png"]
     }
 })
 
